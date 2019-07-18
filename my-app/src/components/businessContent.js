706b11a29dd2e8cfa4adbd27e31aa44044f864f0
Harsha Vardhan <harsha.vardhan@ascenaretail.com>
2019-07-18 02:23:34 -0400

    harsha-dayam: create component for business content

---
+++ my-app/src/components/businessContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function businessContent() {
+  return (
+    <div className='businessContent'>
+      Shop for Men's Oxford shoes online at DSW, where we carry a variety of designer shoes. Shop for Men's Oxford shoes online at DSW, where we carry a variety of designer shoes.
+    </div>
+  );
+}
+
+export default businessContent;
