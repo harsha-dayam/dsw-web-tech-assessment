8fd9b76a5c1656a13754c90e15b4b8325176e572
Harsha Vardhan <harsha.vardhan@ascenaretail.com>
2019-07-18 02:25:11 -0400

    harsha-dayam: import business-content component and styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/header';
+import BusinessContent from './components/businessContent';
 import ProductCardContainer from './containers/productCardContainer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <BusinessContent />
       <ProductCardContainer />
     </div>
   );
