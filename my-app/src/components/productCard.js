18ed9b9879f55372571c423061cef48e39afccad
Harsha Vardhan <harsha.vardhan@ascenaretail.com>
2019-07-18 00:51:44 -0400

    harsha-dayam: create markup for product card

--- my-app/src/components/productCard.js
+++ my-app/src/components/productCard.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import '../App.css';
+import '../App.scss';
 
-function productCard() {
+function productCard({ product:{ id, brand, name, price, compValue, rating, activeColor, colors } }) {
+  const starWidth = ( rating / 5 ) * 100,
+        ratingStarsEmpty = {
+          width: `${starWidth}%`
+        };
+
+  colors.forEach(function(color){
+    color.style = {
+      backgroundColor: color.displayName
+    };
+    color.colorClass = "productColor";
+
+    if(color.id === activeColor) {
+      color.colorClass = "productColor selected"
+    }
+  });
+
   return (
-    <div className="productCard">
-      green
+    <div className='productCard'>
+      <div className='imageHolder'>
+        <img className='productImage' src={require(`../images/products/${id}.jpg`)} alt='shoe' />
+      </div>
+      <div className='productDetails'>
+        <p className='productBrand'>{brand}</p>
+        <p className='productName'>{name}</p>
+        <p className='productPrice'>${price}</p>
+        { compValue && 
+          <p className='productCompVal'>Comp. value ${compValue}.00</p> }
+        { rating &&
+          <p className='productRating'>
+            <span className='ratingStarsEmpty'>★★★★★</span>
+            <span className='ratingStarsFill' style={ratingStarsEmpty}>★★★★★</span>
+          </p> }
+        <div class='productColors'>
+          {colors.map((color) => {        
+            return (<span className={color.colorClass} key={color.id}><button style={color.style}></button></span>)
+          })}
+        </div>  
+      </div>
     </div>
   );
 }
