a6050faf9f081210d679cba92248a8f38b18278c
Harsha Vardhan <harsha.vardhan@ascenaretail.com>
2019-07-18 01:46:22 -0400

    harsha-dayam: create header markup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/header';
 import ProductCardContainer from './containers/productCardContainer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ProductCardContainer />
     </div>
   );
---
+++ my-app/src/components/header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function header() {
+  return (
+    <div className='header'>
+      <div className='mainNav'>
+        <p className='brandLogoContainer'>
+            <img className='brandLogo' src={require(`../images/logo.png`)} alt='brand logo'/>
+        </p>
+        <ul className='secondaryNav'>
+            <li className='account'>
+                <img className='accountImage' src={require(`../images/account.gif`)} alt='account icon'/>
+            </li>
+            <li className='store'>
+                <img className='storeImage' src={require(`../images/store.gif`)} alt='account icon'/>
+            </li>
+            <li className='search'>
+                <img className='searchImage' src={require(`../images/search.gif`)} alt='account icon'/>
+            </li>
+            <li className='bag'>
+                <img className='bagImage' src={require(`../images/bag.gif`)} alt='account icon'/>
+            </li>
+        </ul>
+      </div>
+      <div className='primaryNav'>
+          <ul>
+              <li>Women</li>
+              <li>Men</li>
+              <li>Kids</li>
+              <li>Accesories</li>
+              <li>Clearance</li>
+              <li>Brands</li>
+          </ul>
+      </div>
+    </div>
+  );
+}
+
+export default header;
